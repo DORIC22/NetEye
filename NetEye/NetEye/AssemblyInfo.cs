fc73f7a680821a86d829487144d99fcfec53bc7f
DORIC <KozlovMD.Work@yandex.ru>
2023-04-25 20:55:53 +0700

    Add Montserrat.ttf, UI Auth page, add Task.Async on button and check internet connection.

--- NetEye/NetEye/AssemblyInfo.cs
+++ NetEye/NetEye/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat.ttf", Alias = "Main")]
+[assembly: ExportFont("Montserrat_500.ttf", Alias = "Main_500")]
--- NetEye/NetEye/MainPage.xaml.cs
+++ NetEye/NetEye/MainPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace NetEye
@@ -13,6 +15,41 @@ namespace NetEye
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Предупреждение","Перейти по ссылке?", "Перейти", "Отмена");
+            if (answer)
+                await Launcher.OpenAsync(new Uri("http://admin.net-eye.ru"));
+        }
+
+        public bool checkNetworkState()
+        {
+            bool state= false;
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+                state = true;            
+            return state;
+        }
+
+        private async void btn_Auth_Clicked(object sender, EventArgs e)
+        {
+            bool state = checkNetworkState();
+            if (!state) {
+                await DisplayAlert("Упс", "Ошибка интернет соединения", "Ок");
+                return;
+            }
+            aiMain.IsVisible = true;
+            await Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+            });
+            aiMain.IsVisible = false;
+
+
+
         }
     }
 }
